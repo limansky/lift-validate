8d9916b32c16228050395c8ebc10a523b06ac5e0
Mike Limansky <mike.limansky@gmail.com>
2014-01-16 02:26:32 +0400

    Add Int and Equeals validators.

--- src/main/scala/net/liftmodules/validate/Validators.scala
+++ src/main/scala/net/liftmodules/validate/Validators.scala
@@ -20,6 +20,8 @@ import net.liftweb.http.S
 import net.liftweb.http.js.JE._
 import net.liftweb.json.{ JObject }
 import net.liftweb.json.JsonDSL._
+import net.liftweb.json.JObject
+import net.liftweb.json.JField
 
 object Validators extends Validators
 
@@ -42,13 +44,13 @@ trait Validators {
 
     override def validate() = {
       if (isEnabled() && (value() == null || value().trim() == "")) {
-//        S.error(errorMessage)
+        //        S.error(errorMessage)
         false
       } else true
     }
 
     override def check: JObject = {
-        "required" -> true
+      "required" -> true
     }
 
     override def messages: Option[JObject] = {
@@ -64,7 +66,7 @@ trait Validators {
       val v = if (value() == null) "" else value().trim()
       // FIXME: Really stupid validation
       if (v != "" && !v.matches("[^@]+@[^@.]+\\.[^@]+")) {
-//        S.error(errorMessage)
+        //        S.error(errorMessage)
         false
       } else true
     }
@@ -72,6 +74,60 @@ trait Validators {
     override def check: JObject = "email" -> true
 
     override def messages: Option[JObject] = errorMessage map (m => "email" -> m)
+  }
+
+  case class ValidateInt(min: Option[Int], max: Option[Int],
+      override val value: () => String, errorMessage: Option[String])(override implicit val ctx: ValidateContext) extends Validate {
+
+    override def validate() = {
+      val trimmed = value().trim()
+      val res = if (!trimmed.isEmpty) {
+        val ival = trimmed.toInt
+        min.map(_ <= ival).getOrElse(true) && max.map(_ >= ival).getOrElse(true)
+      } else true
+      //      if (!res)
+      //        S.error(errorMessage)
+      res
+    }
+
+    override def check: JObject = {
+      val minr = min map (v => JField("min", v))
+      val maxr = max map (v => JField("max", v))
+      (minr, maxr) match {
+        case (Some(mi), Some(ma)) => "range" -> List(mi, ma)
+        case (Some(mi), None) => "min" -> mi
+        case (None, Some(ma)) => "max" -> ma
+        case (None, None) => "number" -> true
+      }
+    }
+
+    override def messages: Option[JObject] = errorMessage map (m => {
+      val minr = min map (v => JField("min", v))
+      val maxr = max map (v => JField("max", v))
+      (minr, maxr) match {
+        case (Some(mi), Some(ma)) => "range" -> m
+        case (Some(mi), None) => "min" -> m
+        case (None, Some(ma)) => "max" -> m
+        case (None, None) => "number" -> m
+      }
+    })
+  }
+
+  case class ValidateEquals(override val value: () => String,
+      val expected: () => String,
+      selector: String,
+      errorMessage: Option[String])(override implicit val ctx: ValidateContext) extends Validate {
+
+    override def validate() = {
+      if (value() != expected()) {
+        //S.error(errorMessage)
+        false
+      } else true
+    }
+
+    override def check: JObject = "equalTo" -> selector
+
+    override def messages: Option[JObject] = errorMessage map ("equalTo" -> _)
   }
 
 }
