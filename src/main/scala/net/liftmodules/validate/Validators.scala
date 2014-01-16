603737148ee990570eb4ce9576241ef469aaa36b
Mike Limansky <mike.limansky@gmail.com>
2014-01-16 16:21:54 +0400

    Implement ValidateRemote

--- src/main/scala/net/liftmodules/validate/Validators.scala
+++ src/main/scala/net/liftmodules/validate/Validators.scala
@@ -16,12 +16,12 @@
 package net.liftmodules.validate
 
 import scala.xml._
-import net.liftweb.http.S
+import net.liftweb.http._
 import net.liftweb.http.js.JE._
-import net.liftweb.json.{ JObject }
+import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
-import net.liftweb.json.JObject
-import net.liftweb.json.JField
+import net.liftweb.json.{ JObject, JField, JNull }
+import net.liftweb.common.Full
 
 object Validators extends Validators
 
@@ -39,7 +39,7 @@ trait Validators {
 
   case class ValidateRequired(
       override val value: () => String,
-      errorMessage: Option[String],
+      errorMessage: Option[String] = None,
       isEnabled: () => Boolean = () => true)(override implicit val ctx: ValidateContext) extends Validate {
 
     override def validate() = {
@@ -58,9 +58,17 @@ trait Validators {
     }
   }
 
+  object ValidateRequired {
+    def apply(value: () => String, errorMessage: String, isEnabled: () => Boolean)(implicit ctx: ValidateContext): ValidateRequired =
+      ValidateRequired(value, Some(errorMessage), isEnabled)
+
+    def apply(value: () => String, errorMessage: String)(implicit ctx: ValidateContext): ValidateRequired =
+      ValidateRequired(value, Some(errorMessage))
+  }
+
   case class ValidateEmail(
       override val value: () => String,
-      errorMessage: Option[String])(override implicit val ctx: ValidateContext) extends Validate {
+      errorMessage: Option[String] = None)(override implicit val ctx: ValidateContext) extends Validate {
 
     override def validate() = {
       val v = if (value() == null) "" else value().trim()
@@ -76,8 +84,12 @@ trait Validators {
     override def messages: Option[JObject] = errorMessage map (m => "email" -> m)
   }
 
-  case class ValidateInt(min: Option[Int], max: Option[Int],
-      override val value: () => String, errorMessage: Option[String])(override implicit val ctx: ValidateContext) extends Validate {
+  object ValidateEmail {
+    def apply(value: () => String, errorMessage: String)(implicit ctx: ValidateContext): ValidateEmail =
+      ValidateEmail(value, Some(errorMessage))
+  }
+
+  case class ValidateInt(min: Option[Int], max: Option[Int], override val value: () => String, errorMessage: Option[String] = None)(override implicit val ctx: ValidateContext) extends Validate {
 
     override def validate() = {
       val trimmed = value().trim()
@@ -91,9 +103,7 @@ trait Validators {
     }
 
     override def check: JObject = {
-      val minr = min map (v => JField("min", v))
-      val maxr = max map (v => JField("max", v))
-      (minr, maxr) match {
+      (min, max) match {
         case (Some(mi), Some(ma)) => "range" -> List(mi, ma)
         case (Some(mi), None) => "min" -> mi
         case (None, Some(ma)) => "max" -> ma
@@ -102,9 +112,7 @@ trait Validators {
     }
 
     override def messages: Option[JObject] = errorMessage map (m => {
-      val minr = min map (v => JField("min", v))
-      val maxr = max map (v => JField("max", v))
-      (minr, maxr) match {
+      (min, max) match {
         case (Some(mi), Some(ma)) => "range" -> m
         case (Some(mi), None) => "min" -> m
         case (None, Some(ma)) => "max" -> m
@@ -113,10 +121,21 @@ trait Validators {
     })
   }
 
+  object ValidateInt {
+    def apply(min: Option[Int], max: Option[Int], value: () => String, errorMessage: String)(implicit ctx: ValidateContext): ValidateInt =
+      ValidateInt(min, max, value, Some(errorMessage))
+
+    def apply(value: () => String, errorMessage: String)(implicit ctx: ValidateContext): ValidateInt =
+      ValidateInt(None, None, value, Some(errorMessage))
+
+    def apply(value: () => String)(implicit ctx: ValidateContext): ValidateInt =
+      ValidateInt(None, None, value)
+  }
+
   case class ValidateEquals(override val value: () => String,
       val expected: () => String,
       selector: String,
-      errorMessage: Option[String])(override implicit val ctx: ValidateContext) extends Validate {
+      errorMessage: Option[String] = None)(override implicit val ctx: ValidateContext) extends Validate {
 
     override def validate() = {
       if (value() != expected()) {
@@ -130,4 +149,46 @@ trait Validators {
     override def messages: Option[JObject] = errorMessage map ("equalTo" -> _)
   }
 
+  object ValidateEquals {
+    def apply(value: () => String, expected: () => String, selector: String, errorMessage: String)(implicit ctx: ValidateContext): ValidateEquals =
+      ValidateEquals(value, expected, selector, Some(errorMessage))
+  }
+
+  case class ValidateRemote(
+      override val value: () => String,
+      func: String => (Boolean, Option[String]))(override implicit val ctx: ValidateContext) extends Validate {
+
+    override def validate() = {
+      func(value())._1
+    }
+
+    override def check: JObject = {
+      import S.NFuncHolder
+
+      val asyncFunc: () => LiftResponse = {
+        case _ =>
+          S.request match {
+            case Full(req) =>
+              val obj: JValue = req.param(fieldName).map(v => {
+                println(v)
+                val (r, m) = func(v)
+                if (r) {
+                  JBool(true)
+                } else {
+                  m map (s => JString(s)) getOrElse JBool(false)
+                }
+              }).getOrElse(JNull)
+              JsonResponse(obj)
+            case _ => JsonResponse(JNull)
+          }
+      }
+
+      S.fmapFunc(NFuncHolder(asyncFunc)) { key =>
+        val url = S.encodeURL(S.contextPath + "/" + LiftRules.ajaxPath) + "?" + key + "=_"
+        "remote" -> url
+      }
+    }
+
+    override def messages: Option[JObject] = None
+  }
 }
